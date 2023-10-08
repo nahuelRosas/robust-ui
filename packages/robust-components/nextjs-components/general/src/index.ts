23a56b5a2bfa7f2fc695797504a08e29a9784d59
Nahuel Rosas <nahuel.rosas@mi.unc.edu.ar>
2023-10-08 19:25:45 -0300

    fixed dependencies

--- packages/robust-components/nextjs-components/general/src/index.ts
+++ packages/robust-components/nextjs-components/general/src/index.ts
@@ -1,17 +1,43 @@
+// "@robust-ui/nested-styled-text": "workspace:*",
+// "@robust-ui/use-global-context": "workspace:*",
+// "@robust-ui/use-clean-value": "workspace:*",
+// "@robust-ui/use-language": "workspace:*",
+// "@robust-ui/constructor": "workspace:*",
+// "@robust-ui/next-link": "workspace:*",
+// "@robust-ui/use-toast": "workspace:*",
+// "@robust-ui/code-text": "workspace:*",
+// "@robust-ui/provider": "workspace:*",
+// "@robust-ui/header": "workspace:*",
+// "@robust-ui/cover": "workspace:*",
+// "@robust-ui/aside": "workspace:*",
+// "@robust-ui/image": "workspace:*",
+// "@robust-ui/block": "workspace:*",
+// "@robust-ui/theme": "workspace:*",
+// "@robust-ui/menu": "workspace:*",
+// "@robust-ui/span": "workspace:*",
+// "@robust-ui/icon": "workspace:*",
+// "@robust-ui/flex": "workspace:*",
+// "@robust-ui/grid": "workspace:*",
+// "@robust-ui/text": "workspace:*"
+
 export * from "@robust-ui/nested-styled-text";
+export * from "@robust-ui/use-global-context";
+export * from "@robust-ui/use-clean-value";
+export * from "@robust-ui/use-language";
 export * from "@robust-ui/constructor";
 export * from "@robust-ui/next-link";
+export * from "@robust-ui/use-toast";
+export * from "@robust-ui/code-text";
 export * from "@robust-ui/provider";
 export * from "@robust-ui/header";
-export * from "@robust-ui/block";
 export * from "@robust-ui/cover";
+export * from "@robust-ui/aside";
 export * from "@robust-ui/image";
-export * from "@robust-ui/text";
+export * from "@robust-ui/block";
+export * from "@robust-ui/theme";
+export * from "@robust-ui/menu";
 export * from "@robust-ui/span";
+export * from "@robust-ui/icon";
 export * from "@robust-ui/flex";
 export * from "@robust-ui/grid";
-export * from "@robust-ui/icon";
-export * from "@robust-ui/aside";
-export * from "@robust-ui/use-language";
-export * from "@robust-ui/menu";
-export * from "@robust-ui/code-text";
+export * from "@robust-ui/text";
