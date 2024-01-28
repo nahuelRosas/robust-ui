99307f7c97203a84de4b7d0faaa1228a6180166c
Nahuel Rosas <nahuel.rosas@mi.unc.edu.ar>
2024-01-28 00:39:37 -0300

    Update code formatting and fix minor bugs. @robust-ui/cover

---
+++ packages/components/custom/cover/src/types.ts
@@ -0,0 +1,38 @@
+import { StyledTextProps } from "@robust-ui/styled-text";
+import { Ticon, IconProps } from "@robust-ui/icon";
+import { colors } from "@robust-ui/theme";
+import {
+  EnhancedPropsNoGeneric,
+  PartialOrNestedPartial,
+  PartialOrNestedPartialArray,
+  EnhancedProps,
+} from "@robust-ui/constructor";
+
+export interface CoverProps extends EnhancedProps<HTMLElement> {
+  paragraph?: PartialOrNestedPartial<string>;
+  paragraphProps?: StyledTextProps;
+  label?: PartialOrNestedPartial<string>;
+  labelProps?: StyledTextProps;
+  icons?: PartialOrNestedPartialArray<
+    | {
+        color?: keyof typeof colors;
+        colorRaw?: string;
+        type: Ticon;
+      }
+    | Ticon
+  >;
+  iconProps?: IconProps;
+}
+
+export interface CoverPropsNoGeneric
+  extends EnhancedPropsNoGeneric<HTMLElement> {
+  label?: string;
+  paragraph?: string;
+  icons?:
+    | {
+        color?: keyof typeof colors;
+        colorRaw?: string;
+        type: Ticon;
+      }[]
+    | Ticon[];
+}
