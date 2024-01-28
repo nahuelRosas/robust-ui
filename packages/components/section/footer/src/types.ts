c6a42238f0e3c259a31d963b192a522512df8234
Nahuel Rosas <nahuel.rosas@mi.unc.edu.ar>
2024-01-28 00:20:55 -0300

    Update code formatting and fix minor bugs. @robust-ui/footer

---
+++ packages/components/section/footer/src/types.ts
@@ -0,0 +1,6 @@
+import { EnhancedProps, EnhancedPropsNoGeneric } from "@robust-ui/constructor";
+
+export interface FooterProps extends EnhancedProps<HTMLElement> {}
+
+export interface FooterPropsNoGeneric
+  extends EnhancedPropsNoGeneric<HTMLElement> {}
