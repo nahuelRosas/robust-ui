941a5d006e80c25d11b3a3c55fcb6d13b23765bc
Nahuel Rosas <nahuel.rosas@mi.unc.edu.ar>
2024-01-28 00:20:01 -0300

    Update code formatting and fix minor bugs. @robust-ui/header

---
+++ packages/components/section/header/src/types.ts
@@ -0,0 +1,6 @@
+import { EnhancedProps, EnhancedPropsNoGeneric } from "@robust-ui/constructor";
+
+export interface HeaderProps extends EnhancedProps<HTMLElement> {}
+
+export interface HeaderPropsNoGeneric
+  extends EnhancedPropsNoGeneric<HTMLElement> {}
