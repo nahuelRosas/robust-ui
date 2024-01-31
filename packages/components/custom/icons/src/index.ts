de20807377fb10e9dbc3efdb2809a6873f24ed54
Nahuel Rosas <nahuel.rosas@mi.unc.edu.ar>
2024-01-31 19:44:36 -0300

    Add importIcon function to dynamically import icons

--- packages/components/custom/icons/src/index.ts
+++ packages/components/custom/icons/src/index.ts
@@ -1,6 +1,12 @@
 import { TIcons } from "./types";
 export * from "./types";
 
+/**
+ * Imports an icon from the icons module based on the provided key.
+ * If the import fails, it falls back to the errorWarningFill icon.
+ * @param key - The key of the icon to import.
+ * @returns The imported icon.
+ */
 export async function importIcon({ key }: { key: keyof TIcons }) {
   try {
     const icon = (await import(`./icons/index.js`)) as TIcons;
